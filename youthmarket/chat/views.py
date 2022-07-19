d1c3403467988afe7e101ed51e7224c0f7a732c5
kyunghwan1207 <gkw1207@hufs.ac.kr>
2022-07-19 21:34:45 +0900

    Feat : "로그인 기능구현 세션으로 유지구현"

--- youthmarket/chat/views.py
+++ youthmarket/chat/views.py
@@ -6,9 +6,10 @@ import json
 def index(request):
     return render(request, 'index.html', {})
 
-def room(request, room_name):
+def room(request, room_info):
+    # room_info_list = room_info.split('&') # [post_idx, sellerIdx.idx, myInfo.idx]
     return render(request, 'room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
+        'room_name_json': mark_safe(json.dumps(room_info))
     })
 # def chat(request, post_id):
 #     return render(request, 'chat.html', {
